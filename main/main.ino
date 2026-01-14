65a86507b8f23c1d55ee2df2b46301f84e810128
Zain Kergaye <zain4utah@gmail.com>
2026-01-13 17:48:26 -0700

    Updated code

--- main/main.ino
+++ main/main.ino
@@ -1,22 +1,38 @@
 #include <Servo.h>
-#include <Ultrasonic.h>
 
-#define ULTRASONIC_TRIG 12 
-#define ULTRASONIC_EMITTER 13
 #define SERVO_PIN 11
+#define MIC_COUNT 3 
 
-Ultrasonic ultrasonic(ULTRASONIC_TRIG, ULTRASONIC_EMITTER); 
 Servo arm0;
+int[] mic_pins = {10, 9, 8};
+
 
 void setup() {
   arm0.attach(SERVO_PIN);
+  Serial.begin(9600);
 }
 
 void loop() {
- arm0.write(map(ultrasonic.read(), 0, 10, 0, 180));
-  // arm0.write(0);
-  // delay(1000);
-  // arm0.write(180);
-  // delay(1000);
+  arm0.write(0);
+  Serial.println(0);
+  delay(5000);
 }
 
+
+/* 
+* localization_algorithm - Takes an array of signals with samples and threshold. 
+* Returns the angle it came from
+* signal - array of signals 
+* samples - number of samples 
+* threshold - minimum signal noise
+* 
+* returns int
+*/
+static int localization_algorithm(double signal[], int samples, double threshold) {
+
+  for(int i = 0; i < samples; i++) {
+     
+  }
+
+  return 0;
+}
